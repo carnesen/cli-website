416b6d44957bedb0301140d8620ba5271e2de221
Chris Arnesen <chris.arnesen@gmail.com>
2020-06-20 05:32:48 -0700

    Add "autocomplete" feature

--- src/show-command.ts
+++ src/show-command.ts
@@ -1,31 +1,36 @@
 import { CliCommand, CliOneOfArgGroup } from '@carnesen/cli';
 
-export const show = CliCommand({
+export const showCommand = CliCommand({
 	name: 'show',
 	description: 'Show the source code of a command',
 	positionalArgGroup: CliOneOfArgGroup({
 		values: [
+			'.' as const,
 			'echo' as const,
 			'history' as const,
 			'multiply' as const,
-			'throw-error' as const,
 			'show' as const,
+			'throw-error' as const,
 		],
 		placeholder: '<command>',
 		required: true,
 	}),
-	action(_command) {
+	action(name) {
 		let url: string;
-		switch (_command) {
+		switch (name) {
 			case 'echo':
 			case 'multiply':
 			case 'throw-error': {
-				url = `https://github.com/carnesen/cli/blob/master/examples/src/${_command}/index.ts`;
+				url = `https://github.com/carnesen/cli/blob/master/examples/src/${name}/index.ts`;
+				break;
+			}
+			case '.': {
+				url = `https://github.com/carnesen/cli-website/blob/master/src/root-branch.ts`;
 				break;
 			}
 			case 'history':
 			case 'show': {
-				url = `https://github.com/carnesen/cli-website/blob/master/src/${_command}.ts`;
+				url = `https://github.com/carnesen/cli-website/blob/master/src/${name}-command.ts`;
 				break;
 			}
 			default: {
